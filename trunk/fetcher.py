c7594f73ed3816d9316ae96c817c09fec6d45a30
scuq <scuq@1a7a2bed-621e-0410-9e14-afe992aec4fa>
2006-09-27 16:49:54 +0000

    test

--- trunk/fetcher.py
+++
@@ -1,7 +0,0 @@
-class py_tvm_fetcher:
-
-	def __init__(self, dateToGet):
-		self.dateToGet = date
-
-	def getChannelTvm(self,channel):
-		return channel
--- trunk/svn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export SVN_SSH="ssh -l scuq"
-umask 002
-
