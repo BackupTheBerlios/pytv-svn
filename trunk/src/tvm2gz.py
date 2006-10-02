214506c0a6e8f3d647ea20d12eafbfe954b39e93
shp <shp@1a7a2bed-621e-0410-9e14-afe992aec4fa>
2006-10-02 19:40:15 +0000

    Now tvm2gz contains classes

--- trunk/src/tvm2gz.py
+++ trunk/src/tvm2gz.py
@@ -1,43 +1,64 @@
-#!/usr/bin/python2.4
+# Copyright (C) 2006  Stefan Nistelberger (scuq@kages.at)
+#		      Hans-Peter Schadler (blade.runner@gmx.at)
+#		      Daniel Schrammel (nowx@gmx.at)
+# tv_grab_de_tvmovie.py - tv movie grabber
+# convert_tvm_gz_xmlv.py - module### - converts a tvm file over gz to xml
 
-def convertTvmGz(tvmfile,gzfile):
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 
-	f = open(tvmfile, "rb")
-	g = open(gzfile, "wb")
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-	f.seek(0x006a,0)
-	g.write(f.read(6))
-	f.seek(0x0064,0)
-	g.write(f.read(6))
-	f.seek(0x0070,0)
-	g.write(f.read(88))
-	f.seek(0x0320,0)
-	g.write(f.read(17))
-	f.seek(0x0011,0)
-	g.write(f.read(83))
-	f.seek(0x012c,0)
-	g.write(f.read(100))
-	f.seek(0x0000,0)
-	g.write(f.read(17))
-	f.seek(0x0331,0)
-	g.write(f.read(183))
-	f.seek(0x0258,0)
-	g.write(f.read(200))
-	f.seek(0x0190,0)
-	g.write(f.read(200))
-	f.seek(0x00c8,0)
-	g.write(f.read(100))
-	f.seek(0x03e8,0)
-	g.write(f.read())
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-	f.close()
-	g.close()
-	
-def main():
-	tvmfile = "test.tvm"
-	gzfile = "test2.gz"
-	print("Converting " + tvmfile + " to " + gzfile)
-	
-	convertTvmGz(tvmfile,gzfile)
-	
-if __name__=='__main__': main()
+class convert_tvm_gz:
+	def __init__(self, tvmfile, gzfile):
+		self.tvmfile = tvmfile
+		self.gzfile = gzfile
+
+		f = open(tvmfile, "rb")
+		g = open(gzfile, "wb")
+
+		f.seek(0x006a,0)
+		g.write(f.read(6))
+		f.seek(0x0064,0)
+		g.write(f.read(6))
+		f.seek(0x0070,0)
+		g.write(f.read(88))
+		f.seek(0x0320,0)
+		g.write(f.read(17))
+		f.seek(0x0011,0)
+		g.write(f.read(83))
+		f.seek(0x012c,0)
+		g.write(f.read(100))
+		f.seek(0x0000,0)
+		g.write(f.read(17))
+		f.seek(0x0331,0)
+		g.write(f.read(183))
+		f.seek(0x0258,0)
+		g.write(f.read(200))
+		f.seek(0x0190,0)
+		g.write(f.read(200))
+		f.seek(0x00c8,0)
+		g.write(f.read(100))
+		f.seek(0x03e8,0)
+		g.write(f.read())
+
+		f.close()
+		g.close()
+
+		return 0
+		
+class extract_xml:
+	def __init__(self, gzfile, xmlfile):
+		self.gzfile = gzfile
+		self.xmlfile = xmlfile
+		
+		return xmlfile
